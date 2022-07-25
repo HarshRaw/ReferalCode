5a44f3f4616bfa99aab1ecfcd745ae26cf1debba
Manoj Kumar Sharma <mailme@manojkumarsharma.com>
2022-07-25 10:48:55 +0530

    Added the C# Console .NET Core 3.1 Project

---
+++ cs_con_LibMgmtSys/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_con_LibMgmtSys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
